5925419ca63e5fba2b7371e98dcaad4eb56a9ae8
Yannick <yannick1691@users.noreply.github.com>
2017-11-19 00:01:02 +0100

    Added comments to _contact.scss and wrote html and css for speeches page

--- dev/js/script.js
+++ dev/js/script.js
@@ -6,8 +6,29 @@ const dropdownMenu = document.querySelector('.navigation--hamburger');
 // The dropdown function
 function toggleDropdown() {
   dropdownMenu.classList.toggle('open');
-}
+};
 
 // Event listners for Hamburger menu
 hamburgerButton.addEventListener('click', toggleDropdown);
 // End Hamburger menu.
+
+// Speeches Toggle more text
+// Select the elements
+const showMoreBut = document.querySelector('#showMore');
+const moreText = document.querySelector('.moreText');
+
+// Check if the button excists (to prevent errors)
+if (showMoreBut !== null && showMoreBut !== undefined) {
+  // Toggle Function
+  function toggleShowMore() {
+    if (moreText.classList.contains('moreText--Open')) {
+      showMoreBut.innerHTML = 'lees verder...';
+    }
+    else {
+      showMoreBut.innerHTML = 'minder weergeven...';
+    }
+    moreText.classList.toggle('moreText--Open');
+  };
+  // Add event listeners
+  showMoreBut.addEventListener('click', toggleShowMore);
+}
